81a61ea7bcf3fbe479987b4956394ee5e942791f
Martin Dufour <dufm2911@usherbrooke.ca>
2022-11-10 13:26:49 -0500

    Emptied repo<

--- Server/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { PartieModule } from './partie.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(PartieModule);
-  await app.listen(3000);
-}
-bootstrap();
