465024c52a1afed325873a435cc69532d53c556f
Jessy Provencher <jessyprovencher@tuta.io>
2022-11-30 18:04:02 -0500

    WIP Location Controller/Service
    Begin Location on the backend

--- BroomitApi/Models/BroomitDatabaseSettings.cs
+++ BroomitApi/Models/BroomitDatabaseSettings.cs
@@ -5,4 +5,5 @@ public class BroomitDatabaseSettings
     public string ConnectionString { get; set; } = null!;
     public string DatabaseName { get; set; } = null!;
     public string UsersCollectionName { get; set; } = null!;
+    public string LocationsCollectionName { get; set; } = null!;
 }
---
+++ BroomitApi/Services/LocationService.cs
@@ -0,0 +1,49 @@
+﻿using MongoDB.Driver;
+using BroomitApi.Models;
+using Microsoft.Extensions.Options;
+using Location = BroomitModels.Location;
+using Task = System.Threading.Tasks.Task;
+
+namespace BroomitApi.Services;
+
+public class LocationService
+{
+    private readonly IMongoCollection<Location> _locationsCollection;
+
+    public LocationService(IOptions<BroomitDatabaseSettings> broomitDbSettings)
+    {
+        MongoClient mongoClient = new(broomitDbSettings.Value.ConnectionString);
+        IMongoDatabase database = mongoClient.GetDatabase(broomitDbSettings.Value.DatabaseName);
+
+        _locationsCollection = database.GetCollection<Location>(broomitDbSettings.Value.LocationsCollectionName);
+    }
+
+    public async Task<List<Location>> GetLocationsAsync()
+    {
+        return await _locationsCollection.Find(location => true).ToListAsync();
+    }
+
+    public async Task<Location>? GetLocationAsync(string id)
+    {
+        return await _locationsCollection.Find(location => location.Id == id).FirstOrDefaultAsync();
+    }
+
+    public async Task<Location> CreateLocationAsync(Location location)
+    {
+        // TODO: geolocation (?)
+        await _locationsCollection.InsertOneAsync(location);
+        return location;
+    }
+
+    public async Task UpdateLocationAsync(string id, Location locationIn)
+    {
+        // TODO: geolocation (?)
+        await _locationsCollection.ReplaceOneAsync(location => location.Id == id, locationIn);
+    }
+
+    public async Task<bool> RemoveLocationAsync(string id)
+    {
+        DeleteResult result = await _locationsCollection.DeleteOneAsync(location => location.Id == id);
+        return result.DeletedCount == 1;
+    }
+}
