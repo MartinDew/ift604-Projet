c3d93a67f90f7f7d4ccdf255504e3d4a6289d75a
Jessy Provencher <jessyprovencher@tuta.io>
2022-12-09 17:59:36 -0500

    Add 'Back' button on FindLocationActivity

--- BroomitClient/app/src/main/java/projet/ift604/broomitclient/ui/find_location/FindLocationActivity.kt
+++ BroomitClient/app/src/main/java/projet/ift604/broomitclient/ui/find_location/FindLocationActivity.kt
@@ -1,9 +1,7 @@
 package projet.ift604.broomitclient.ui.find_location
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -12,7 +10,6 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import projet.ift604.broomitclient.R
 import projet.ift604.broomitclient.databinding.ActivityFindLocationBinding
-import projet.ift604.broomitclient.ui.locations.LocationCreationActivity
 
 class FindLocationActivity : AppCompatActivity(), OnMapReadyCallback  {
     private lateinit var binding: ActivityFindLocationBinding
@@ -20,6 +17,8 @@ class FindLocationActivity : AppCompatActivity(), OnMapReadyCallback  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_location)
+
+        supportActionBar?.title = getString(R.string.prompt_find_location)
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
