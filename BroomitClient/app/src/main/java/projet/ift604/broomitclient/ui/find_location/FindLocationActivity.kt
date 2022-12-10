cdd54eb8f1c2b90a0d5326c54da1799fd5826c60
Jessy Provencher <jessyprovencher@tuta.io>
2022-12-09 22:09:10 -0500

    WIP Marker with external API

--- BroomitClient/app/src/main/java/projet/ift604/broomitclient/ui/find_location/FindLocationActivity.kt
+++ BroomitClient/app/src/main/java/projet/ift604/broomitclient/ui/find_location/FindLocationActivity.kt
@@ -1,18 +1,34 @@
 package projet.ift604.broomitclient.ui.find_location
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.gson.JsonObject
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import projet.ift604.broomitclient.ApplicationState
 import projet.ift604.broomitclient.R
 import projet.ift604.broomitclient.databinding.ActivityFindLocationBinding
 
 class FindLocationActivity : AppCompatActivity(), OnMapReadyCallback  {
     private lateinit var binding: ActivityFindLocationBinding
+
+    private var displayName: String = ""
+
+    private var latitude: Double = 0.0
+    private var longitude: Double = 0.0
+
+    private var gMap: GoogleMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +39,51 @@ class FindLocationActivity : AppCompatActivity(), OnMapReadyCallback  {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
 
+        val submit: Button = findViewById(R.id.submit)
+        submit.setOnClickListener {
+            val handler = CoroutineExceptionHandler { _, err -> err.printStackTrace() }
+            lifecycleScope.launch(Dispatchers.IO + handler) {
+                val state = ApplicationState.instance
+                try {
+                    val query: String = findViewById<EditText?>(R.id.query).text.toString()
+                    val response = state.nominatim(query)
+                    // TODO: Afficher tous les résultats obtenus
+                    val result = response?.get(0) as JsonObject
+
+                    displayName = result["display_name"].toString()
+                    latitude = (result.getAsJsonPrimitive("lat").asString).toDouble()
+                    longitude = (result.getAsJsonPrimitive("lon").asString).toDouble()
+
+                    runOnUiThread {
+                        // Effacer la position précédente
+                        gMap?.clear()
+
+                        val latLng = LatLng(latitude, longitude)
+
+                        // Créer un nouveau marqueur
+                        val markerOptions = MarkerOptions()
+                        markerOptions.position(latLng)
+
+                        // Donner un titre au marqueur avec la latitude/longitude obtenue
+                        markerOptions.title("(" + latLng.latitude.toString() + "; " + latLng.longitude.toString() + ")")
+
+                        // Ajouter le marqueur à la carte
+                        gMap?.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+                        gMap?.addMarker(markerOptions)
+                    }
+                } catch (e: Exception) {
+                    // TODO
+                    System.out.println(e.message)
+                }
+                // TODO
+            }
+        }
     }
 
 override fun onMapReady(googleMap: GoogleMap) {
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        gMap = googleMap
+
+        // Afficher les contrôles pour zoomer
+        gMap!!.uiSettings.isZoomControlsEnabled = true
     }
 }
--- BroomitClient/app/src/main/java/projet/ift604/broomitclient/ui/locations/LocationCreationActivity.kt
+++ BroomitClient/app/src/main/java/projet/ift604/broomitclient/ui/locations/LocationCreationActivity.kt
@@ -2,23 +2,15 @@ package projet.ift604.broomitclient.ui.locations
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import com.google.gson.JsonObject
 import projet.ift604.broomitclient.ApplicationState
 import projet.ift604.broomitclient.R
 import projet.ift604.broomitclient.databinding.ActivityCreateLocationBinding
 import projet.ift604.broomitclient.models.Location
 import projet.ift604.broomitclient.ui.find_location.FindLocationActivity
-import java.math.BigDecimal
 
 class LocationCreationActivity : AppCompatActivity() {
 
@@ -39,30 +31,6 @@ class LocationCreationActivity : AppCompatActivity() {
         binding.findLocation.setOnClickListener {
             val intent = Intent(this, FindLocationActivity::class.java)
             startActivity(intent)
-
-            // TODO: Déplacer cette logique dans l'activity reliée
-            /*
-            val handler = CoroutineExceptionHandler { _, err -> err.printStackTrace() }
-            lifecycleScope.launch(Dispatchers.IO + handler) {
-                val state = ApplicationState.instance
-                try {
-                    // TODO: Utiliser l'adresse fournie par l'utilisateur
-                    val response = state.nominatim("Université de Sherbrooke")
-                    // TODO: Afficher tous les résultats obtenus
-                    val result = response?.get(0) as JsonObject
-                    val displayName = result["display_name"].toString()
-                    val latitude = BigDecimal(result.getAsJsonPrimitive("lat").asString)
-                    val longitude = BigDecimal(result.getAsJsonPrimitive("lon").asString)
-
-                    binding.notes.setText("$displayName\n$latitude\n$longitude")
-
-                } catch (e: Exception) {
-                    // TODO
-                    System.out.println(e.message)
-                }
-                // TODO
-            }
-            */
         }
 
         binding.save.setOnClickListener {
